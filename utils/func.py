7b90b74ddd9a1da05190b0f0e36c315ce6000a88
Orcale oMii <oracle.omii@gmail.com>
2025-04-01 23:57:55 +0530

    Update func.py

--- utils/func.py
+++ utils/func.py
@@ -1,6 +1,4 @@
-# Copyright (c) 2025 devgagan : https://github.com/devgaganin.  
-# Licensed under the GNU General Public License v3.0.  
-# See LICENSE file in the repository root for full license text.
+
 
 import concurrent.futures
 import time
